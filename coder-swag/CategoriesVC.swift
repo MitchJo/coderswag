13943ef59953349079c79fc1bf08554809022df7
MitchJo <mitchell@exploreembedded.com>
2018-08-12 16:51:48 +0530

    🎊 Completed 🎉

--- coder-swag/CategoriesVC.swift
+++ coder-swag/CategoriesVC.swift
@@ -42,8 +42,12 @@ class CategoriesVC: UIViewController, UITableViewDataSource,
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let productsVC = segue.destination as? ProductsVC{
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
             assert(sender as? Category != nil)
             productsVC.initProducts(category: sender as! Category)
+        
         }
     }
     
